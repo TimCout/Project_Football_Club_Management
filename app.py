4b5376cb314c5cf0cb548d13af18f23b475b3365
laura2909 <laura.mussard@edu.ece.fr>
2024-11-01 13:18:29 +0100

    page modifier

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify,request, render_template
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
@@ -8,6 +8,7 @@ mongo_client = MongoClient("mongodb://localhost:27017/")
 db = mongo_client["database"]
 clubs_collection = db["clubs"]
 teams_collection = db["teams"]
+players_collection = db["players"]
 
 @app.route('/api/clubs', methods=['GET'])
 def get_clubs():
@@ -15,6 +16,48 @@ def get_clubs():
     clubs = clubs_collection.find({}, {"_id": 1, "name": 1})
     club_list = [{"id": str(club["_id"]), "name": club["name"]} for club in clubs]
     return jsonify(club_list)
+
+
+#récupérer les équipes d'un club
+@app.route('/api/teams/by-club-name/<club_name>', methods=['GET'])
+def get_teams_by_club_name(club_name):
+    # Recherche du club par nom
+    club = clubs_collection.find_one({"name": club_name})
+    
+    if not club:
+        return jsonify({"error": "Club not found"}), 404
+    
+    club_id = club["_id"]
+
+    # Recherche des équipes liées à ce club
+    teams = teams_collection.find({"club_id": club_id}, {"_id": 1, "name": 1})
+    
+    # Formatage de la réponse pour inclure les identifiants et noms des équipes
+    team_list = [{"id": str(team["_id"]), "name": team["name"]} for team in teams]
+    return jsonify(team_list)
+
+#récupérer les joueurs d'une équipe et d'un club
+@app.route('/api/players/by-club-team', methods=['GET'])
+def get_players_by_club_and_team():
+    club_name = request.args.get('club')
+    team_name = request.args.get('team')
+    
+
+    # Trouver le club par nom pour obtenir son ID
+    club = clubs_collection.find_one({"name": club_name})
+    if not club:
+        return jsonify({"error": "Club not found"}), 404
+    
+
+    # Trouver l'équipe par nom et club_id
+    team = teams_collection.find_one({"name": team_name, "club_id": club["_id"]})
+    if not team:
+        return jsonify({"error": "Team not found"}), 404
+
+    # Récupérer les joueurs de l'équipe et du club
+    players = players_collection.find({"team_id": team["_id"], "club_id": club["_id"]}, {"_id": 1, "name": 1})
+    player_list = [{"id": str(player["_id"]), "name": player["name"]} for player in players]
+    return jsonify(player_list)
 
 @app.route('/api/teams/<club_id>', methods=['GET'])
 def get_teams(club_id):
@@ -35,6 +78,7 @@ def get_teams(club_id):
 
     return jsonify(team_list)
 
+
 @app.route('/api/players/<team_id>', methods=['GET'])
 def get_players(team_id):
     print(f"API endpoint '/api/players/{team_id}' was hit.")
@@ -50,9 +94,213 @@ def get_players(team_id):
     player_list = [{"id": str(player["_id"]), "name": player["name"]} for player in players]
     return jsonify(player_list)
 
+
+
+#récupère l'id du joueur en fonction de son nom, team et club
+def get_player_id(name):
+    user = players_collection.find_one({"name": name}, {"_id": 1})
+    if user:
+        return user["_id"]
+    else:
+        print(f"Aucun joueur trouvé avec le nom '{name}'.")
+        return None
+
+#récupère l'id de la team en fonction de son nom et club
+def get_team_id(name):
+    user = teams_collection.find_one({"name": name}, {"_id": 1})
+    if user:
+        return user["_id"]
+    else:
+        print(f"Aucune team trouvée avec le nom '{name}'.")
+        return None
+    
+#récupère l'id du club en fonction de son nom
+def get_club_id(name):
+    user = clubs_collection.find_one({"name": name}, {"_id": 1})
+    if user:
+        return user["_id"]
+    else:
+        print(f"Aucun club trouvé avec le nom '{name}'.")
+        return None
+    
+
+# mettre à jour le nom d'un joueur
+@app.route('/update_player_name', methods=['POST'])
+def update_player_name():
+    data = request.json
+    name = data.get('name')
+    new_name = data.get('new_name')
+    player_id = get_player_id(name)
+
+    if not player_id:
+        return jsonify({"error": "Player not found"}), 404
+
+    result = players_collection.update_one(
+        {"_id": player_id},
+        {"$set": {"name": new_name}}
+    )
+    if result.matched_count > 0:
+        return jsonify({"message": f"Player '{name}' updated successfully."})
+    else:
+        return jsonify({"error": "Update failed"}), 500
+
+#supprimer ET ajouter un joueur dans une team
+def update_player_from_team(club_id, team_id, club_id_new, team_id_new, player_name):
+    try:
+        
+        # Rechercher le joueur dans la collection players
+        player = players_collection.find_one({"name": player_name, "team_id": ObjectId(team_id), "club_id": ObjectId(club_id)})
+        #player_new = players_collection.find_one({"name": player_name, "team_id": ObjectId(team_id_new), "club_id": ObjectId(club_id_new)})
+        
+        if not player:
+            return {"error": "Player not found in the specified team."}
+        
+
+        # Mettre à jour la collection teams en retirant le joueur de la liste des joueurs de l'équipe
+        teams_collection.update_one(
+            {"_id": ObjectId(team_id)},
+            {"$pull": {"players": player["_id"]}}
+        )
+
+        teams_collection.update_one(
+            {"_id": ObjectId(team_id_new)},
+            {"$push": {"players": player["_id"]}}
+        )
+
+        
+        return {"message": f"Player '{player_name}' removed successfully from team '{team_id}'."}
+    
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
+    
+# mettre à jour la team d'un joueur
+@app.route('/update_player_team', methods=['POST'])
+def update_player_team():
+    data = request.json
+    name = data.get('name')
+    team = data.get('from_team')
+    club = data.get('from_club')
+    new_team = data.get('to_team')
+    new_club = data.get('to_club')
+
+    # Obtenez les identifiants nécessaires pour les équipes et les clubs
+    new_team_id = get_team_id(new_team)
+    team_id = get_team_id(team)
+    player_id = get_player_id(name)
+    new_club_id = get_club_id(new_club)
+    club_id = get_club_id(club)
+
+    if not player_id:
+        return jsonify({"error": "Player not found"}), 404
+    if not team_id or not new_team_id:
+        return jsonify({"error": "Team(s) not found"}), 404
+    if not club_id or not new_club_id:
+        return jsonify({"error": "Club(s) not found"}), 404
+
+    # Retirer le joueur de l'équipe actuelle et l'ajouter à la nouvelle
+    update_player_from_team(club_id, team_id,new_club_id, new_team_id, name)
+
+    # Mettre à jour l'identifiant de l'équipe du joueur
+    result = players_collection.update_one(
+        {"_id": player_id},
+        {"$set": {"team_id": new_team_id}}
+    )
+
+    if result.matched_count > 0:
+        return jsonify({"message": f"Player '{name}' successfully moved to new team '{new_team}' in club '{new_club}'."})
+    else:
+        return jsonify({"error": "Update failed"}), 500
+
+
+# mettre à jour le club d'un joueur
+@app.route('/update_player_club', methods=['POST'])
+def update_player_club():
+    data = request.json
+    name = data.get('name')
+    team = data.get('from_team')
+    club = data.get('from_club')
+    new_team = data.get('to_team')
+    new_club = data.get('to_club')
+
+    player_id = get_player_id(name)
+    if not player_id:
+        return jsonify({"error": "Player not found"}), 404
+
+    new_team_id = get_team_id(new_team)
+    new_club_id = get_club_id(new_club)
+    if not new_team_id or not new_club_id:
+        return jsonify({"error": "New team or club not found"}), 404
+
+    result = players_collection.update_one(
+        {"_id": player_id},
+        {"$set": {"team_id": new_team_id, "club_id": new_club_id}}
+    )
+
+    if result.matched_count > 0:
+        return jsonify({"message": f"Player '{name}' updated successfully."})
+    else:
+        return jsonify({"error": "Update failed "}), 500
+
+    # Appel de la fonction pour déplacer le joueur
+    update_player_team(name, team, club, new_club, new_team)
+
+    
+
+# mettre à jour le nom d'une team
+@app.route('/update_team_name', methods=['POST'])
+def update_team_name():
+    data = request.json
+    team = data.get('from_team')
+    new_name = data.get('to_team')
+
+    team_id = get_team_id(team)
+    if not team_id:
+        return jsonify({"error": "Team not found"}), 404
+
+    # Mettre à jour le nom de l'équipe
+    result = teams_collection.update_one(
+        {"_id": team_id},
+        {"$set": {"name": new_name}}
+    )
+
+    if result.matched_count > 0:
+        return jsonify({"message": f"Team '{team}' successfully renamed to '{new_name}'."})
+    else:
+        return jsonify({"error": "Update failed"}), 500
+
+# mettre à jour le nom d'un club
+@app.route('/update_club_name', methods=['POST'])
+def update_club_name():
+    data = request.json
+    club = data.get('from_club')
+    new_name = data.get('to_club')
+
+    club_id = get_club_id(club)
+    if not club_id:
+        return jsonify({"error": "Club not found"}), 404
+
+    # Mettre à jour le nom du club
+    result = clubs_collection.update_one(
+        {"_id": club_id},
+        {"$set": {"name": new_name}}
+    )
+
+    if result.matched_count > 0:
+        return jsonify({"message": f"Club '{club}' successfully renamed to '{new_name}'."})
+    else:
+        return jsonify({"error": "Update failed"}), 500
+
+
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/modify-element.html')
+def modifier():
+    return render_template('modify-element.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- services/databasemanager.py
+++ services/databasemanager.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
 import redis
 from bson.objectid import ObjectId
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 
 # Connexion à MongoDB et Redis
 mongo_client = MongoClient("mongodb://localhost:27017/")
@@ -10,6 +14,7 @@ db = mongo_client["database"]
 clubs_collection = db["clubs"]
 players_collection = db["players"]
 teams_collection = db["teams"]
+
 
 def add_club(club_data):
     try:
@@ -228,213 +233,6 @@ response_delete_club = delete_club_by_name("ZOUM")
 print(response_delete_club)
 """
 
-
-
-#récupère l'id du joueur en fonction de son nom, team et club
-def get_player_id(name):
-    user = players_collection.find_one({"name": name}, {"_id": 1})
-    if user:
-        return user["_id"]
-    else:
-        print(f"Aucun joueur trouvé avec le nom '{name}'.")
-        return None
-
-#récupère l'id de la team en fonction de son nom et club
-def get_team_id(name):
-    user = teams_collection.find_one({"name": name}, {"_id": 1})
-    if user:
-        return user["_id"]
-    else:
-        print(f"Aucune team trouvée avec le nom '{name}'.")
-        return None
-    
-#récupère l'id du club en fonction de son nom
-def get_club_id(name):
-    user = clubs_collection.find_one({"name": name}, {"_id": 1})
-    if user:
-        return user["_id"]
-    else:
-        print(f"Aucun club trouvé avec le nom '{name}'.")
-        return None
-
-
-
-#modifier le nom d'un joueur
-def update_mongo_player_name(name,new_name):
-
-    player_id = get_player_id(name)
-
-    result = players_collection.update_one(
-        {"_id": player_id},
-        {"$set": {"name": new_name}}
-    )
-    
-    if result.matched_count > 0:
-        print(f"Le joueur avec l'id {player_id} est mis à jour.")
-    else:
-        print(f"Aucun joueur trouvé avec l'id {player_id}.")
-
-#update_mongo_player_name("P", "Lionel")
-
-
-#supprimer un joueur d'une team
-def remove_player_from_team(club_id, team_id, player_name):
-    try:
-        # Rechercher l'équipe pour s'assurer qu'elle appartient au club spécifié
-        team = teams_collection.find_one({"_id": ObjectId(team_id), "club_id": ObjectId(club_id)})
-        
-        if not team:
-            return {"error": "Team not found in the specified club."}
-
-        # Rechercher le joueur dans la collection players
-        player = players_collection.find_one({"name": player_name, "team_id": ObjectId(team_id), "club_id": ObjectId(club_id)})
-        
-        if not player:
-            return {"error": "Player not found in the specified team."}
-        
-
-        # Mettre à jour la collection teams en retirant le joueur de la liste des joueurs de l'équipe
-        teams_collection.update_one(
-            {"_id": ObjectId(team_id)},
-            {"$pull": {"players": player["_id"]}}
-        )
-
-        
-        return {"message": f"Player '{player_name}' removed successfully from team '{team['name']}'."}
-    
-    except Exception as e:
-        return {"error": str(e)}
-
-def add_player_to_team_update(club_id, team_id, player_data):
-    try:
-        team = teams_collection.find_one({"_id": ObjectId(team_id), "club_id": ObjectId(club_id)})
-        
-        if not team:
-            return {"error": "Team not found in the specified club."}
-        
-    
-        player_data["club_id"] = ObjectId(club_id)
-        player_data["team_id"] = ObjectId(team_id)
-        
-        player_id = players_collection.insert_one(player_data).inserted_id
-        
-        teams_collection.update_one(
-            {"_id": ObjectId(team_id)},
-            {"$push": {"players": player_id}}
-        )
-        
-        player_data_str = {k: str(v) if isinstance(v, ObjectId) else v for k, v in player_data.items()}
-        player_data_str["_id"] = str(player_id)
-        
-        redis_client.hset(f"player:{player_id}", mapping=player_data_str)
-        
-        return {"message": "Player added successfully", "player_id": str(player_id)}
-    
-    except Exception as e:
-        return {"error": str(e)}
-#modifier la team d'un joueur
-def update_mongo_player_team(name, team ,club, new_club, new_team):
-
-    new_team_id = get_team_id(new_team)
-    team_id = get_team_id(team)
-
-    player_id = get_player_id(name)
-
-    player_data = {"name": name}
-
-    
-
-
-    new_club_id = get_club_id(new_club)
-    club_id = get_club_id(club)
-
-    remove_player_from_team(club_id, team_id, name)
-    add_player_to_team_update(new_club_id, new_team_id, player_data)
-    
-
-
-
-    result = players_collection.update_one(
-        {"_id": player_id},
-        {"$set": {"team_id": new_team_id}}
-    )
-
-
-    
-    if result.matched_count > 0:
-        print(f"Le joueur avec l'id {player_id} est mis à jour.")
-    else:
-        print(f"Aucun joueur trouvé avec l'id {player_id}.")
-
-    
-
-#update_mongo_player_team("LIONNEL","U21", "PSG", "PSG", "U20")
-
-
-#modifier le club et la team d'un joueur
-def update_mongo_player_club(name,team, club, new_team, new_club ):
-
-    player_id = get_player_id(name)
-    new_team_id = get_team_id(new_team)
-    new_club_id = get_club_id(new_club)
-
-    result = players_collection.update_one(
-        {"_id": player_id},
-        {"$set": {"team_id": new_team_id,
-                  "club_id": new_club_id}}
-    )
-
-    update_mongo_player_team(name, team, club ,new_club_id, new_team)
-    
-    if result.matched_count > 0:
-        print(f"Le joueur avec l'id {player_id} est mis à jour.")
-    else:
-        print(f"Aucun joueur trouvé avec l'id {player_id}.")
-
-#update_mongo_player_club("NATHAN","U80", "ZOUM", "U20", "PSG" )
-
-
-#modifier le nom d'une team
-
-def update_mongo_team_name(team, new_name):
-
-    team_id =  get_team_id(team)
-
-    result = teams_collection.update_one(
-        {"_id": team_id},
-        {"$set": {"name": new_name}}
-    )
-    
-    if result.matched_count > 0:
-        print(f"La team avec l'id {team_id} est mise à jour.")
-    else:
-        print(f"Aucune team trouvée avec l'id {team_id}.")
-
-#update_mongo_team_name("U20", "U40")
-
-
-
-
-
-#modifier le nom d'un club
-
-
-def update_mongo_club_name(club, new_name):
-
-    club_id = get_club_id(club)
-
-    result = clubs_collection.update_one(
-        {"_id": club_id},
-        {"$set": {"name": new_name}}
-    )
-    
-    if result.matched_count > 0:
-        print(f"Utilisateur avec user_id {club_id} mis à jour.")
-    else:
-        print(f"Aucun utilisateur trouvé avec user_id {club_id}.")
-
-    
-#update_mongo_club_name("PS", "SP")
 
 
 def get_club_with_teams_and_players(club_name):
